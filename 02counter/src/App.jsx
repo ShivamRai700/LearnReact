103660c8c203920dee5cd9272e92d927fbd1f728
Shivam Rai <22shivamrai@gmail.com>
2025-08-31 14:56:25 +0530

    learnt on day 3 , 4 ,5 ,6, 7

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -13,9 +13,23 @@ function App() {
   let addValue = function () {
     // counter = counter + 1;
     if (counter >= 19) {
-      counter = 19;
+      prevCounter = 19;
     }
-    setCounter(counter + 1);
+
+    // Reason for below code : Har click par value sirf ek hi bar update hogi, due to useState jo batches mein updates bhejta hai.Due to all lines doing same work , they are sent in one batch.
+
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+
+    // If and only if you want to use as such : do this : where prevCounter is parameter which is taken by the callback inside the setCounter Method Call. i.e. prevCounter is holding the previous value of counter.
+
+    setCounter(prevCounter => prevCounter + 1);
+    setCounter(prevCounter => prevCounter + 1);
+    setCounter(prevCounter => prevCounter + 1);
+    setCounter(prevCounter => prevCounter + 1);
+
   }
 
   let removeValue = () => {
@@ -29,7 +43,6 @@ function App() {
     <>
       <h1>Chai aur React</h1>
       <h2>Counter value : {counter}</h2>
-
       <button onClick={addValue}>Add Value</button>
       <br />
       <button onClick={removeValue}>Remove Value</button>
